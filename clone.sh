a0941d9f9579c62bbde9e9f40a4de671ce472b1e
Saanvi S <mrnags697@gmail.com>
2025-11-17 11:32:50 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -3,6 +3,17 @@
 REPOS=(
     "https://github.com/s-saanvi/e1.git"
     "https://github.com/s-saanvi/e2.git"
+        "https://github.com/s-saanvi/e3.git"
+    "https://github.com/s-saanvi/e4.git"
+        "https://github.com/s-saanvi/e5.git"
+    "https://github.com/s-saanvi/e6.git"
+        "https://github.com/s-saanvi/e7.git"
+    "https://github.com/s-saanvi/e8.git"
+        "https://github.com/s-saanvi/e9.git"
+    "https://github.com/s-saanvi/e10.git"
+        "https://github.com/s-saanvi/e11.git"
+    "https://github.com/s-saanvi/e12.git"
+    "https://github.com/s-saanvi/e13.git"
 )
 
 for REPO in "${REPOS[@]}"; do
